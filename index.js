7e45ed59873e998ae9950a019c8342a276493aba
Doreen Schwurack <doreen.schwurack@gmx.de>
2026-04-09 13:31:03 +0200

    added eventListener prev + next Buttons

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-// let maxPage = 1;
+let maxPage = 1;
 let page = 1;
 const searchQuery = "";
 
@@ -29,6 +29,9 @@ async function fetchCharacters(indexPage) {
       throw new Error(`Failed to fetch Data! Status Code: ${response.status}`);
     }
     const data = await response.json();
+
+    maxPage = data.info.pages;
+
     // console.log(data.info.pages, page);
     // if (data.info.pages === page) {
     //   maxPages = true;
@@ -45,6 +48,26 @@ async function fetchCharacters(indexPage) {
   // }
 }
 fetchCharacters(page);
+console.log(maxPage);
+
+nextButton.addEventListener("click", () => {
+  if (page === maxPage) {
+    alert("Hey, this is the End! :( Hold your breath and count to ten.");
+    console.log(page, maxPage);
+    return;
+  }
+  page++;
+  fetchCharacters(page);
+});
+
+prevButton.addEventListener("click", () => {
+  if (page === 1) {
+    alert("You reached the start of the universe. :D");
+    return;
+  }
+  page--;
+  fetchCharacters(page);
+});
 
 // async function handleFetchCharacters() {
 //   const result = await fetchCharacters();
